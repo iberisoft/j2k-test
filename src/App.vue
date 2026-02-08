783c1aab254066124c813d401505ead1d30302af
Pavel Zaytsev <pavel@iberisoft.com>
2026-02-08 18:14:33 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as cornerstone from 'cornerstone-core'
+import * as cornerstoneWadoImageLoader from 'cornerstone-wado-image-loader'
+import * as dicomParser from 'dicom-parser'
+
+cornerstoneWadoImageLoader.external.cornerstone = cornerstone;
+cornerstoneWadoImageLoader.external.dicomParser = dicomParser;
+
+const image = ref(null);
+
+onMounted(async () => {
+  image.value = await cornerstone.loadAndCacheImage('wadouri:http://localhost:8080/good-image.dcm');
+});
+</script>
+
+<template>
+  <div id="app">
+    <h1 v-if="image">Image loaded: {{image.width}} x {{image.height}} pixels</h1>
+  </div>
+</template>
